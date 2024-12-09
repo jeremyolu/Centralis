7cdbd15e189c3dedb184a5003d834d6ecae0874e
Jeremy Olu <jeremy-olu@hotmail.com>
2024-12-09 20:53:01 +0000

    login window for application

--- Centralis.App/MainWindow.xaml.cs
+++ Centralis.App/MainWindow.xaml.cs
@@ -1,24 +1,92 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Centralis.App
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AboutMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ServiceStatusMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void SupportMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void RestartMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to restart Centralis?", "Restart", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Get the current application's executable path
+                string exePath = Process.GetCurrentProcess().MainModule.FileName;
+
+                // Start a new instance of the application
+                Process.Start(exePath);
+
+                // Close the current application
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void PinTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextNumeric(e.Text);
+        }
+
+        private void UserIdTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextCharacters(e.Text);
+        }
+
+        private bool IsTextCharacters(string text)
+        {
+            return Regex.IsMatch(text, "^[a-zA-Z.]+$");
+        }
+
+        private bool IsTextNumeric(string text)
+        {
+            return Regex.IsMatch(text, "^[0-9]+$");
         }
     }
 }
